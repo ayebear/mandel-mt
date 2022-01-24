0a8067ed77a2d8ccff836690a9bcc445d83cc581
Eric Hebert <ayebear@gmail.com>
2022-01-23 21:42:47 -0500

    Colors working

--- src/main.rs
+++ src/main.rs
@@ -19,23 +19,38 @@ fn mandel(x: f32, y: f32, iter: u32) -> u32 {
 
 fn main() {
     let max_iterations = 1024u32;
-    let img_side = 128u32;
+    let img_side = 512u32;
     let cxmin = -2f32;
     let cxmax = 1f32;
     let cymin = -1.5f32;
     let cymax = 1.5f32;
     let scalex = (cxmax - cxmin) / img_side as f32;
     let scaley = (cymax - cymin) / img_side as f32;
+    let base = ((max_iterations - 1) as f32).log10();
     // Create a new ImgBuf
     let mut imgbuf = image::ImageBuffer::new(img_side, img_side);
     // Calculate for each pixel
     for (x, y, pixel) in imgbuf.enumerate_pixels_mut() {
+        // Get iteration count
         let cx = cxmin + x as f32 * scalex;
         let cy = cymin + y as f32 * scaley;
         let i = mandel(cx, cy, max_iterations);
-        let col = ((i as f32 / max_iterations as f32) * 256f32).floor() as u8;
-        // *pixel = image::Luma([col]);
-        *pixel = image::Rgb([col, col, col]);
+        let mut col = image::Rgb([0u8, 0u8, 0u8]);
+        // Convert iteration count to pixel color
+        if i < max_iterations - 1 {
+            let c = (3.0 * (i as f32).log10()) / base;
+            if c < 1.0 {
+                col[2] = (255.0 * c) as u8;
+            } else if c < 2.0 {
+                col[1] = (255.0 * (c - 1.0)) as u8;
+                col[2] = 255u8;
+            } else {
+                col[0] = 255u8;
+                col[1] = (255.0 * (c - 2.0)) as u8;
+                col[2] = 255u8;
+            }
+        }
+        *pixel = col;
     }
     // Save image
     imgbuf.save("fractal.png").unwrap();
