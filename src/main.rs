0a21c5ab65164a1f54edceb47eb102b320db8981
Eric Hebert <ayebear@gmail.com>
2022-01-24 01:02:22 -0500

    still working, using buffer

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,12 @@ extern crate image;
 extern crate num_complex;
 
 use num_complex::Complex;
+use rayon::prelude::*;
 
-fn mandel(x: f32, y: f32, iter: u32) -> u32 {
+fn mandel(x: f64, y: f64, iter: u64) -> u64 {
     let c = Complex::new(x, y);
-    let mut z = Complex::new(0f32, 0f32);
-    let mut i = 0u32;
+    let mut z = Complex::new(0f64, 0f64);
+    let mut i = 0u64;
     for t in 0..iter {
         if z.norm() > 2.0 {
             break;
@@ -18,40 +19,56 @@ fn mandel(x: f32, y: f32, iter: u32) -> u32 {
 }
 
 fn main() {
-    let max_iterations = 1024u32;
+    let max_iterations = 1024u64;
     let img_size = 512u32;
-    let cxmin = -2f32;
-    let cxmax = 1f32;
-    let cymin = -1.5f32;
-    let cymax = 1.5f32;
-    let scalex = (cxmax - cxmin) / img_size as f32;
-    let scaley = (cymax - cymin) / img_size as f32;
-    let base = ((max_iterations - 1) as f32).log10();
+    let cxmin = -2f64;
+    let cxmax = 1f64;
+    let cymin = -1.5f64;
+    let cymax = 1.5f64;
+    let scalex = (cxmax - cxmin) / img_size as f64;
+    let scaley = (cymax - cymin) / img_size as f64;
+    let base = ((max_iterations - 1) as f64).log10();
     // Create a new ImgBuf
-    let mut imgbuf = image::ImageBuffer::new(img_size, img_size);
+    // let mut imgbuf = image::ImageBuffer::new(img_size, img_size);
+    // Create buffer
+    // let buf_size = (img_size * img_size * 4) as usize;
+    let imgbuf = image::RgbaImage::new(img_size, img_size);
+    // let mut buffer = vec![P::Subpixel; buf_size];
+    let mut buffer = imgbuf.into_raw();
+    // let mut buffer: [u8; buf_size] = [0; buf_size];
     // Calculate for each pixel
-    for (x, y, pixel) in imgbuf.enumerate_pixels_mut() {
-        // Get iteration count
-        let cx = cxmin + x as f32 * scalex;
-        let cy = cymin + y as f32 * scaley;
-        let i = mandel(cx, cy, max_iterations);
-        let mut col = image::Rgb([0u8, 0u8, 0u8]);
-        // Convert iteration count to pixel color
-        if i < max_iterations - 1 {
-            let c = (3.0 * (i as f32).log10()) / base;
-            if c < 1.0 {
-                col[2] = (255.0 * c) as u8;
-            } else if c < 2.0 {
-                col[1] = (255.0 * (c - 1.0)) as u8;
-                col[2] = 255u8;
-            } else {
-                col[0] = 255u8;
-                col[1] = (255.0 * (c - 2.0)) as u8;
-                col[2] = 255u8;
+    buffer
+        .chunks_mut((img_size * 4) as usize)
+        .enumerate()
+        .for_each(|(y, row)| {
+            for x in 0..img_size {
+                // Get iteration count
+                let cx = cxmin + x as f64 * scalex;
+                let cy = cymin + y as f64 * scaley;
+                let i = mandel(cx, cy, max_iterations);
+                let mut col = image::Rgba([0u8, 0u8, 0u8, 255u8]);
+                // Convert iteration count to pixel color
+                if i < max_iterations - 1 {
+                    let c = (3.0 * (i as f64).log10()) / base;
+                    if c < 1.0 {
+                        col[2] = (255.0 * c) as u8;
+                    } else if c < 2.0 {
+                        col[1] = (255.0 * (c - 1.0)) as u8;
+                        col[2] = 255u8;
+                    } else {
+                        col[0] = 255u8;
+                        col[1] = (255.0 * (c - 2.0)) as u8;
+                        col[2] = 255u8;
+                    }
+                }
+                // imgbuf.put_pixel(x, y, col);
+                row[(x * 4) as usize] = col[0];
+                row[(x * 4 + 1) as usize] = col[1];
+                row[(x * 4 + 2) as usize] = col[2];
+                row[(x * 4 + 3) as usize] = col[3];
             }
-        }
-        *pixel = col;
-    }
+        });
     // Save image
-    imgbuf.save("fractal.png").unwrap();
+    let img = image::RgbaImage::from_raw(img_size, img_size, buffer).unwrap();
+    img.save("fractal.png").unwrap();
 }
