a95415adea90fac66cf9e0de0e4398542ef216bf
Eric Hebert <ayebear@gmail.com>
2022-01-23 21:07:02 -0500

    Split to separate function

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,20 @@ extern crate num_complex;
 
 use num_complex::Complex;
 
+fn mandel(x: f32, y: f32, iter: u32) -> u32 {
+    let c = Complex::new(x, y);
+    let mut z = Complex::new(0f32, 0f32);
+    let mut i = 0u32;
+    for t in 0..iter {
+        if z.norm() > 2.0 {
+            break;
+        }
+        z = z * z + c;
+        i = t;
+    }
+    return i;
+}
+
 fn main() {
     let max_iterations = 1024u32;
     let img_side = 128u32;
@@ -18,16 +32,7 @@ fn main() {
     for (x, y, pixel) in imgbuf.enumerate_pixels_mut() {
         let cx = cxmin + x as f32 * scalex;
         let cy = cymin + y as f32 * scaley;
-        let c = Complex::new(cx, cy);
-        let mut z = Complex::new(0f32, 0f32);
-        let mut i = 0u32;
-        for t in 0..max_iterations {
-            if z.norm() > 2.0 {
-                break;
-            }
-            z = z * z + c;
-            i = t;
-        }
+        let i = mandel(cx, cy, max_iterations);
         let col = ((i as f32 / max_iterations as f32) * 256f32).floor() as u8;
         // *pixel = image::Luma([col]);
         *pixel = image::Rgb([col, col, col]);
